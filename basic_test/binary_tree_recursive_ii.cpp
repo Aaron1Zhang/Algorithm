a430a1481f5fda73c2947f73b3cd4d637c8b1b15
zh <ysu72@gmail.com>
2021-04-15 15:39:00 +0800

    change the binary_tree_recursive_ii.cpp

--- basic_test/binary_tree_recursive_ii.cpp
+++ basic_test/binary_tree_recursive_ii.cpp
@@ -1,1 +1,26 @@
-#include <ios
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//使用分治法遍历二叉树
+struct TreeNode {
+    int value;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+vector<int> preorder_traverse(TreeNode* root) {
+    vector<int> res;
+    if(root == nullptr) {
+        return res;
+    }
+    auto leftRes = preorder_traverse(root->left);
+    auto rightRes = preorder_traverse(root->right);
+    res.push_back(root->value);
+    res.insert(res.end(), leftRes.begin(), leftRes.end());
+    res.insert(res.end(), rightRes.begin(), rightRes.end());
+    return res;
+}
+int main(int argc, const char** argv) {
+    return 0;
+}
