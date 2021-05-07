2d054182a5c880f0b23a2922ac54faedbb7879ff
Aaron1Zhang <zh669079108@163.com>
2021-05-07 17:43:13 +0800

    add code to two_sum.cpp

--- basic_test/twoSum_1.cpp
+++ basic_test/twoSum_1.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
 
-int main(int argc, const char** argv) {
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    map<int, int> maps;
+    auto size = nums.size();
+    vector<int> res;
+    for (int i = 0; i < size; ++i)
+    {
+        auto iter = maps.find(target - nums[i]);
+        if (iter != maps.end())
+        {
+            res.push_back(i);
+            res.push_back(iter->second);
+            return res;
+        }
+        maps.emplace(nums[i], i);
+    }
+    return res;
+}
+
+int main(int argc, const char **argv)
+{
     return 0;
 }
