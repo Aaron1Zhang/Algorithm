48443ef5322ea189e3201c081f4936441f517413
Aaron1Zhang <zh669079108@163.com>
2021-05-10 11:33:32 +0800

    add revser Strinfg of word

--- basic_test/reverseString.cpp
+++ basic_test/reverseString.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <iostream>
 using namespace std;
 
 class Solution {
@@ -14,3 +15,28 @@ public:
         }
     }
 };
+
+string reverseWords(string s) {
+    int i = 0,j = 0;
+    int size = s.size();
+    while(j < size) {
+        while(j < size && s[j] != ' ')
+            ++j;
+        auto t = j - 1;
+        while(i < t) {
+            swap(s[i], s[t]);
+            ++i;
+            --t;
+        }
+        ++j;
+        i = j;
+    }
+    return s;
+}
+
+int main(int argc, const char** argv) {
+    string s = "abc de erd e eree";
+    auto res = reverseWords(s);
+    cout << res << endl;
+    return 0;
+}
