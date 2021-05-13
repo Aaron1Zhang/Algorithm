8d5217dc865be340b1bf873105d3c187a45ba178
Aaron1Zhang <zh669079108@163.com>
2021-05-13 23:11:44 +0800

    modify bianry serach insert use template

--- basic_test/binary_search_insert_35.cpp
+++ basic_test/binary_search_insert_35.cpp
@@ -20,4 +20,27 @@ public:
         return left;
 
     }
+
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while(left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] > target) {
+                right = mid;
+            } else if(nums[mid] < target) {
+                left = mid;
+            } else {
+                return mid;
+            }
+        }
+        if(target > nums[right]) {
+            return right + 1;
+        }
+        if(target <= nums[left]) {
+            return left;
+        }
+        return right;
+
+    }
 };
