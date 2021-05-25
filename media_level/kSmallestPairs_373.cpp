cc81962e52ea861024cbc0ce74922fba188785ea
Aaron1Zhang <zh669079108@163.com>
2021-05-25 16:36:48 +0800

    change the minHeap to maxHeap

--- media_level/kSmallestPairs_373.cpp
+++ media_level/kSmallestPairs_373.cpp
@@ -3,25 +3,57 @@ private:
     int size{0};
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        auto lam = [](const pair<int, int>& p1, const pair<int,int>& p2) {
+       /*  auto lam = [](const pair<int, int>& p1, const pair<int,int>& p2) {
             return p1.first + p1.second > p2.first + p2.second;
         };
 
-        priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(lam)>        max_heap(lam);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(lam)>  min_heap(lam);
 
         for(int i = 0; i < nums1.size(); ++i) {
             for(int j = 0 ; j < nums2.size(); ++j) {
                 auto item = make_pair(nums1[i], nums2[j]);
-                max_heap.push(item);
+                min_heap.push(item);
             }
         }
         vector<vector<int>> res;
-        while(!max_heap.empty() && k) {
+        while(!min_heap.empty() && k) {
             --k;
-            auto item = max_heap.top();
+            auto item = min_heap.top();
             res.push_back(vector<int>{item.first, item.second});
-            max_heap.pop();
+            min_heap.pop();
+        }
+        return res; */
+		
+		
+		auto lam = [](const pair<int, int>& p1, const pair<int,int>& p2) {
+             return p1.first + p1.second < p2.first + p2.second;
+        };
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(lam)> max_heap(lam);
+        vector<vector<int>> res;    
+        for(int i = 0; i < nums1.size(); ++i) {
+            for(int j = 0 ; j < nums2.size(); ++j) {
+                auto item = make_pair(nums1[i], nums2[j]);
+                if(size < k) {
+                    ++size;
+                    max_heap.push(item);
+                } else {
+                    auto max_item = max_heap.top();
+                    if((max_item.first+max_item.second) > (item.first + item.second)) {
+                        max_heap.pop();
+                        max_heap.push(item);
+                    }
+                }
+               
+            }
+        }
+        while(!max_heap.empty()) {
+             auto item = max_heap.top();
+             res.push_back(vector<int>{item.first, item.second});
+             max_heap.pop();
+
         }
         return res;
+
     }
 };
