99dad33587fc1f418ab781c52b35e4d84e7870ea
Aaron1Zhang <zh669079108@163.com>
2021-05-28 23:39:27 +0800

    add leetcode 108

---
+++ basic_test/sortedArrayToBST_108.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return helper(nums, 0, nums.size() - 1);
+    }
+
+    TreeNode* helper(vector<int>& nums, int left, int right) {
+        if(left > right) {
+            return nullptr;
+        }
+        auto mid = left + (right - left) / 2;
+        auto head = new TreeNode(nums[mid]);
+        head->left = helper(nums, left, mid - 1);
+        head->right = helper(nums, mid + 1, right);
+
+        return head;
+    }
+};
