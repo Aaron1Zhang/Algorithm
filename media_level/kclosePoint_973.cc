e4bfdb8761d22529d059538f636324626fec4104
Aaron1Zhang <zh669079108@163.com>
2021-10-23 23:18:47 +0800

    add second method of kclosePoint user of quick select

--- media_level/kclosePoint_973.cc
+++ media_level/kclosePoint_973.cc
@@ -24,4 +24,41 @@ public:
     int calcDistance(vector<int>& point) {
         return pow(point[0], 2) + pow(point[1], 2);
     }
-}
+
+    //方法二，快速选择
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>> res;
+        quick_select(points, 0, points.size() - 1, k);
+        res.insert(res.end(), points.begin(), points.begin() + k);
+        return res;
+    }
+    //返回第K小的数
+    void quick_select(vector<vector<int>>& vec, int start, int end, int k) {
+        if (start == end) return ;
+
+        int mid = start + (end - start) / 2;
+        auto pivot = calcDistance(vec[mid]);
+        int i = start;
+        int j = end;
+        while (i <= j) {
+            while (i <= j && calcDistance(vec[i]) < pivot) {
+                ++i;
+            }
+            while (i <= j && calcDistance(vec[j]) > pivot) {
+                --j;
+            }
+            if (i <= j) {
+                std::swap(vec[i], vec[j]);
+                ++i;
+                --j;
+            }
+        }
+        if (k - 1 + start < j) {
+            return quick_select(vec, start, j, k);
+
+        } else if (k - 1 + start >= i) {
+            return quick_select(vec, i, end, k - (i - start));
+        } 
+        return;
+    }
+};
