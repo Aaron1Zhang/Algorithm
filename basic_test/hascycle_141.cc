67efe029f8aab0fa991a4afa9d5c807806724837
Aaron1Zhang <zh669079108@163.com>
2021-11-01 17:19:26 +0800

    use fast and slow pointer to slove the list cycle problem

--- basic_test/hascycle_141.cc
+++ basic_test/hascycle_141.cc
@@ -20,4 +20,20 @@ public:
         return false;   
         
     }
+    bool hasCycle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return false;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (slow != fast) {
+            if (fast == nullptr || fast->next == nullptr) {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return true;
+
+    }
 };
